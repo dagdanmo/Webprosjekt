53728b029e211e6d7afe0c25a8719daceee2e814
dagdanmo <37124101+dagdanmo@users.noreply.github.com>
2019-05-28 17:39:50 +0200

    Triks og mix

--- Hovedmappe/index.js
+++ Hovedmappe/index.js
@@ -10,23 +10,35 @@ window.onload=function(){
 });
 
 var readSignup = document.getElementById("signUp");
-
+var loginField = document.getElementById("loginField");
+var signupForm = document.getElementById("signupForm");
 readSignup.addEventListener("click", function(){
     console.log("Signup klicked");
+    //loginField.style.opacity = "0";
+    //loginField.style.transition = "0%";
+    loginField.style.transitionTimingFunction = "ease-in";
+    loginField.style.transitionDuration = "1s";
+    loginField.style.opacity = "0";
+    loginField.style.top = "0%";
+
+    signupForm.style.top = "15%";
+    signupForm.style.transitionTimingFunction = "ease-in";
+    signupForm.style.transitionDuration = "1s";
+    signupForm.style.opacity = "1";
 });
 
 function createSignupForm(){
-    
+
 };
 
 };
 
                          //Unused elements
 //document.getElementById("myBtn").addEventListener("click", displayDate);
-    
+    /*
     function fadeOut(){
         var loginField = document.getElementById('loginField')
-        loginField.style.opacity = '0';
-        loginField.style.top = '60%';
+        loginField.style.opacity = "0";
+        loginField.style.transition = "%." ;
     } 
-  
+  */
