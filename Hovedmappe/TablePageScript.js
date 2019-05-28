9dabba6035e4383732e574501fa1899894ae4ffd
Mathias <Olberic@users.noreply.github.com>
2019-05-28 13:31:21 +0200

    lots of js suff with tabelpage

--- Hovedmappe/TablePageScript.js
+++ Hovedmappe/TablePageScript.js
@@ -1,9 +1,16 @@
-console.log("In progress");
+var taskBoxId = 0;
 
-var TaskBox = document.getElementById("TaskBox0");
-var addBar = document.getElementById("AddTaskBarButton");
+window.onload = function(){
+    var TaskBox = document.getElementById('TaskBox0');
+    document.getElementById('AddTaskBarButton').addEventListener("click", addTaskBarOnClick);
 
-addBar.addEventListener("click", function(){
-    var newTaskBox = TaskBox.cloneNode(true);
-    TaskBox.appendChild(newTaskBox);
-});
+    // --CURRENT ISSUE --
+    // Clones everything. 2 turns into 4, 4 to 8 etc
+    function addTaskBarOnClick(){
+        taskBoxId++;
+        var newTaskBox = TaskBox.cloneNode(true);
+        TaskBox.appendChild(newTaskBox);
+        console.log("Added bar");
+        console.log(taskBoxId);
+};
+}
