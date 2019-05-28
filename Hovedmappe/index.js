f19bc04bc4dae0cd3d805c5499b7f037ec6a0aa9
Daniel Boylan <42612854+boylan157@users.noreply.github.com>
2019-05-28 14:27:17 +0200

    pikk

--- Hovedmappe/index.js
+++ Hovedmappe/index.js
@@ -8,18 +8,14 @@ window.onload=function(){
         console.log(fetchUsername.value);
         console.log(fetchPassword.value);
 });
-
-var readSignup = document.getElementById("signUp");
-
-readSignup.addEventListener("click", function(){
-    console.log("Signup klicked");
-});
-
-function createSignupForm(){
-
-};
-
 };
 
                          //Unused elements
 //document.getElementById("myBtn").addEventListener("click", displayDate);
+    
+    function fadeOut(){
+        var loginField = document.getElementById('loginField')
+        loginField.style.opacity = '0';
+        loginField.style.top = '60%';
+    } 
+  
