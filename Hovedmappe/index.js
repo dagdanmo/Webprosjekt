23a967c353e0e078678b793dc826a279f804bdc1
Mathias <Olberic@users.noreply.github.com>
2019-05-28 09:53:54 +0200

    Just a couple of simple fixes
    added html, Code spacing etc

--- Hovedmappe/index.js
+++ Hovedmappe/index.js
@@ -1,21 +1,14 @@
 window.onload=function(){
-    
     var fetchUsername = document.getElementById("inputUsername");
-    var fetchPassword = document.getElementById("inputPassword");
-    
+    var fetchPassword = document.getElementById("inputPassword");    
     var loginButtonTest = document.getElementById("loginButton");
 
     loginButtonTest.addEventListener("click", function(){
-
         console.log("Du er logget inn.");
         console.log(fetchUsername.value);
         console.log(fetchPassword.value);
-    
 });
-
-
-
 };
 
-
+                         //Unused elements
 //document.getElementById("myBtn").addEventListener("click", displayDate);
