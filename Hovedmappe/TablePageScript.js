ca6075aeeee745d1d40fe22c62be0c71792182d5
mpnordlien <42611178+mpnordlien@users.noreply.github.com>
2019-05-29 11:34:50 +0200

    added max card check

--- Hovedmappe/TablePageScript.js
+++ Hovedmappe/TablePageScript.js
@@ -3,12 +3,16 @@ window.onload = function(){
     document.getElementById('AddTaskBarButton').addEventListener("click", addTaskBarOnClick);
     var i = 0;
     function addTaskBarOnClick(){
-        var newCard = document.createElement("div");
-        newCard.className = 'newCard';
-        newCard.id = 'newCard'+i;
-        var newCardName = document.createTextNode("newCard");
-        newCard.appendChild(newCardName);
-        document.getElementById("TeamBox").appendChild(newCard);
-        i++;
+        if(i<4){
+            var newCard = document.createElement("div");
+            newCard.className = 'newCard';
+            newCard.id = 'newCard'+i;
+            var newCardName = document.createTextNode("newCard");
+            newCard.appendChild(newCardName);
+            document.getElementById("TeamBox").appendChild(newCard);
+            i++;
+        } else {
+            alert("Max cars reached for current team");
+        }
     };
 }
