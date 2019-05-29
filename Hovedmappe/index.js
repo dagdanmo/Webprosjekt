35788b9e64049ff8da42c8def55dc779944893e1
Daniel Boylan <42612854+boylan157@users.noreply.github.com>
2019-05-29 12:34:32 +0200

    ds

--- Hovedmappe/index.js
+++ Hovedmappe/index.js
@@ -1,4 +1,4 @@
-window.onload=function(){
+
     var fetchUsername = document.getElementById("inputUsername");
     var fetchPassword = document.getElementById("inputPassword");    
     var loginButtonTest = document.getElementById("loginButton");
@@ -69,15 +69,21 @@ confirmInputPassword.addEventListener("input", function(){
 function createSignupForm(){
 
 };
+// Event that happens when user presses "register button when making user"
+var register_Signup = document.getElementById("register_Signup");
+// var readSignup = document.getElementById("signUp");
+register_Signup.addEventListener("click", function(){
+   console.log("Register clicked");
+   //loginField.style.opacity = "0";
+   //loginField.style.transition = "0%";
+   signupForm.style.transitionTimingFunction = "ease-in";
+   signupForm.style.transitionDuration = "0.5s";
+   signupForm.style.opacity = "0";
+   signupForm.style.top = "0%";
 
-};
+   loginField.style.top = "15%";
+   loginField.style.transitionTimingFunction = "ease-in";
+   loginField.style.transitionDuration = "0.5s";
+   loginField.style.opacity = "1";
+}); 
 
-                         //Unused elements
-//document.getElementById("myBtn").addEventListener("click", displayDate);
-    /*
-    function fadeOut(){
-        var loginField = document.getElementById('loginField')
-        loginField.style.opacity = "0";
-        loginField.style.transition = "%." ;
-    } 
-  */
