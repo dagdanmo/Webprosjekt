e5559656e3a60cc46a95db9d41726266316393f6
mpnordlien <42611178+mpnordlien@users.noreply.github.com>
2019-05-29 14:54:30 +0200

    new team

--- Hovedmappe/TablePageScript.js
+++ Hovedmappe/TablePageScript.js
@@ -3,33 +3,49 @@ window.onload = function(){
     //document.getElementById('NewCardButton').addEventListener("click", addTaskBarOnClick);
     
     document.getElementById("NewCardButton").addEventListener("click",addTaskBarOnClick);
-    
+    document.getElementById("NewTeamButton").addEventListener("click",addNewTeamOnClick);
+
+
     var movePerc = 19;
-    var i = 0;
+    var teamI = 0;
+    var boxI = 0;
     
     // keep button on the right side of the new card
     function addNewCardButton(){
         document.getElementById("NewCardButton").style.left = movePerc+"%";
         movePerc = movePerc + 19;
     }
+
+    // creating new teams
+    function addNewTeamOnClick(){
+        var newTeam = document.createElement("div");
+        newTeam.className = "TeamBoxContainer";
+        newTeam.id = "TeamBox"+teamI;
+
+        var newTeamName = document.createTextNode("New Team " + teamI);
+        newTeam.appendChild(newTeamName);
+        document.getElementById("MainBox").appendChild(newTeam);
+
+        teamI++;
+    }
     
     // creating new cards
     function addTaskBarOnClick(){
-        if(i<5){
+        if(boxI<5){
             var newCard = document.createElement("div");
             newCard.className = 'newCard';
-            newCard.id = 'newCard'+i;
-            if(i != 0){
+            newCard.id = 'newCard'+boxI;
+            if(boxI != 0){
                 newCard.style.left = movePerc - 19 +"%";
             }
-            var newCardName = document.createTextNode("newCard");
+            var newCardName = document.createTextNode("New Card " + boxI);
             newCard.appendChild(newCardName);
             document.getElementById("TeamBox").appendChild(newCard);
             
-            i++;
+            boxI++;
             addNewCardButton();
         } else {
-            alert("Max cars reached for current team");
+            alert("Max cards reached for current team");
         }
     };
 
