a6899ab9a6bb232d8f4fcfda67a5fdcd5c9996ad
mpnordlien <42611178+mpnordlien@users.noreply.github.com>
2019-05-29 16:23:34 +0200

    Update TablePageScript.js

--- Hovedmappe/TablePageScript.js
+++ Hovedmappe/TablePageScript.js
@@ -3,7 +3,7 @@ window.onload = function(){
     
     //document.getElementById('NewCardButton').addEventListener("click", addTaskBarOnClick);
     
-    
+    // first create team button.
     var newTeamButton = document.createElement("button");
     newTeamButton.innerHTML = "New Team";
     newTeamButton.type = "button";
@@ -12,30 +12,23 @@ window.onload = function(){
     document.getElementById("MainBox").appendChild(newTeamButton);
     document.getElementById("NewTeamButton").addEventListener("click",addNewTeamOnClick);
 
-    var movePerc = 19;
+    // init
     var teamI = 0;
     var boxI = 0;
 
-
-/*     // keep button on the right side of the new card
-    function addNewCardButton(){
-        document.getElementById("NewCardButton"+ID).style.left = movePerc+"%";
-        movePerc = movePerc + 19;
-    } */
-
     // creating new teams
     function addNewTeamOnClick(){
+        //Creating a new teambox
         var newTeam = document.createElement("div");
         newTeam.className = "TeamBoxContainer";
         newTeam.id = "TeamBox"+teamI;
-
-        //var newTeamName = document.createTextNode("New Team " + teamI);
+        //Name of the new team
         var newTeamName = document.createElement("p");
         newTeamName.innerHTML = "New Team " + teamI;
         newTeamName.className = "NewTeamName";
         newTeam.appendChild(newTeamName);
         document.getElementById("MainBox").appendChild(newTeam);
-
+        //New cardbox button added to the teambox
         var newBoxButton = document.createElement("button");
         newBoxButton.innerHTML = "New Card";
         newBoxButton.className = "buttons";
@@ -48,30 +41,26 @@ window.onload = function(){
     
     // creating new cards
     function addTaskBarOnClick(){
+        
+        // getting the ID from the button pressed (the buttons are called ex. NewCardButton2 pointing to team2)
         var buttonID = event.target.id;
         var ID = buttonID[buttonID.length -1];
         console.log(this);
         console.log(buttonID);
         console.log(ID);
-        //if(boxI<5){
-            var newCard = document.createElement("div");
-            newCard.className = 'NewCard';
-            newCard.id = 'NewCard'+boxI;
-            if(boxI != 0){
-                newCard.style.left = movePerc - 19 +"%";
-            }
-            //var newCardName = document.createTextNode("New Card " + boxI);
-            var newCardName = document.createElement("p");
-            newCardName.innerHTML = "New Card "+ID;
-            newCardName.className = "NewCardName";
-            newCard.appendChild(newCardName);
-            document.getElementById("TeamBox"+ID).appendChild(newCard);
-            
-            //boxI++;
 
-        //} else {
-         //   alert("Max cards reached for current team");
-       // }
+        // creating a new card
+        var newCard = document.createElement("div");
+        newCard.className = 'NewCard';
+        newCard.id = 'NewCard'+boxI;
+
+        // name of the card
+        var newCardName = document.createElement("p");
+        newCardName.innerHTML = "New Card "+ID;
+        newCardName.className = "NewCardName";
+        newCard.appendChild(newCardName);
+        document.getElementById("TeamBox"+ID).appendChild(newCard);
+
     };
 
     
