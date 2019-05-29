3847fa468f02581abe1068aedcfedd1b200e0582
mpnordlien <42611178+mpnordlien@users.noreply.github.com>
2019-05-29 11:32:21 +0200

    added iterator

--- Hovedmappe/TablePageScript.js
+++ Hovedmappe/TablePageScript.js
@@ -1,12 +1,14 @@
 
 window.onload = function(){
     document.getElementById('AddTaskBarButton').addEventListener("click", addTaskBarOnClick);
-
+    var i = 0;
     function addTaskBarOnClick(){
         var newCard = document.createElement("div");
-        newCard.id = 'newCard';
+        newCard.className = 'newCard';
+        newCard.id = 'newCard'+i;
         var newCardName = document.createTextNode("newCard");
         newCard.appendChild(newCardName);
         document.getElementById("TeamBox").appendChild(newCard);
+        i++;
     };
 }
