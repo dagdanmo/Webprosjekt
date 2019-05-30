dc81af3edf3a3443f135cadb19e275702506097c
dagdanmo <37124101+dagdanmo@users.noreply.github.com>
2019-05-30 15:48:58 +0200

    FYYY SØØØØØREN!!!

--- Hovedmappe/CreateTable.js
+++ Hovedmappe/CreateTable.js
@@ -1,15 +1,49 @@
-document.getElementById('AddTaskBarButton').addEventListener("click", addTaskBarOnClick);
-    var i = 0;
-    function addTaskBarOnClick(){
+var createNewTable = document.getElementById("createNewTable");
+var plusSign = document.getElementById("plusSign");
+var confirmTableName = document.getElementById("confirmTableName");
+var inputTableName = document.getElementById("inputTableName");
+
+createNewTable.addEventListener("click", function(){
+    createNewTable.style.transitionTimingFunction = "ease-in";
+    createNewTable.style.transitionDuration = "0.5s";
+    createNewTable.style.top = "200px";
+    createNewTable.style.opacity = "1";
+    
+    inputTableName.style.opacity = "1";
+    inputTableName.style.transitionTimingFunction = "ease-in";
+    inputTableName.style.transitionDuration = "0.5s";
+    inputTableName.style.transitionDelay = "0.4s";
+
+    confirmTableName.style.opacity = "1";
+    confirmTableName.style.opacity = "ease-in";
+    confirmTableName.style.transitionDuration = "0.5s";
+    confirmTableName.style.transitionDelay = "0.4s";
+
+    plusSign.style.opacity = "0";
+    plusSign.style.transitionTimingFunction = "ease-in";
+    plusSign.style.transitionDuration = "0.5s";
+});
+
+    
+    
+    confirmTableName.addEventListener("click", function(){
+
+        var i = 0;
+
         if(i<4){
             var newCard = document.createElement("div");
             newCard.className = 'newCard';
             newCard.id = 'newCard'+i;
-            var newCardName = document.createTextNode("newCard");
+            var newCardName = document.createTextNode(inputTableName.value);
             newCard.appendChild(newCardName);
             document.getElementById("TeamBox").appendChild(newCard);
             i++;
         } else {
-            alert("Max cars reached for current team");
+            alert("Max cards reached for current team");
         }
-    };
+
+
+
+
+    });
+        
