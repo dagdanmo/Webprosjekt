5a7c06aa17a64389e6e668a6569faee45fcb7c91
mpnordlien <42611178+mpnordlien@users.noreply.github.com>
2019-06-03 13:18:49 +0200

    Oppdatering av tablepage merge

--- Hovedmappe/TablePageScript.js
+++ Hovedmappe/TablePageScript.js
@@ -36,35 +36,43 @@ window.onload = function(){
 
 
     // init
-    var teamI = 0;
-    var boxI = 0;
+    var teamIterator = 0;
+    var boxIterator = 0;
+
+    var teamArray=[];
 
     // creating new teams
     function addNewTeamOnClick(){
         
+        //var teamName = prompt("Enter teamname");
         //Creating a new teambox
         var newTeam = document.createElement("div");
-        newTeam.className = "TeamBoxContainer";
-        newTeam.id = "TeamBox"+teamI;
-        console.log(teamName.value);
+        newTeam.className = "teamBoxContainer";
+        newTeam.id = "newTeam"+teamIterator;
+        //teamArray.push({teamIterator, boxIterator});
         
         //Name of the new team
         var newTeamName = document.createElement("p");
         newTeamName.innerHTML = teamName.value;
-        newTeamName.className = "NewTeamName";
-        newTeamName.id = "TeamName"+teamI;
+        newTeamName.className = "teamName";
+        newTeamName.id = "teamBox"+teamIterator;
         newTeam.appendChild(newTeamName);
         document.getElementById("MainBox").appendChild(newTeam);
 
  
         //New cardbox button added to the teambox
-        var newBoxButton = document.createElement("button");
-        newBoxButton.innerHTML = "New Card";
-        newBoxButton.className = "buttons";
-        newBoxButton.id = "NewCardButton"+teamI;
-        newBoxButton.addEventListener("click",addTaskBarOnClick);
-        newTeam.appendChild(newBoxButton);
-
+        var newCardButton = document.createElement("button");
+        newCardButton.innerHTML = "[ + ]";
+        newCardButton.className = "newCardButton";
+        newCardButton.id = "newCardButton"+teamIterator;
+        newCardButton.addEventListener("click",addTaskBarOnClick);
+        newTeam.appendChild(newCardButton);
+        
+        console.log(newTeamName.id);
+        console.log(teamArray);
+        
+        teamIterator++;
+/* 
         // Change name of team
         var changeTeamNameButton = document.createElement("button");
         changeTeamNameButton.innerHTML = "Change team name";
@@ -73,16 +81,15 @@ window.onload = function(){
         changeTeamNameButton.addEventListener("click",changeTeamName);
         newTeamName.appendChild(changeTeamNameButton);
 
-        teamI++;
 
-            // IN PROGRESS
+        // IN PROGRESS
         function changeTeamName(){
         var newValue = prompt("Enter a new name for the card: ");
         console.log(newValue);
         document.getElementById(newTeamName.id);
         newTeamName.innerHTML = newValue;
         }
-    
+     */
     }
 
 
@@ -90,28 +97,44 @@ window.onload = function(){
     function addTaskBarOnClick(){
         
         // getting the ID from the button pressed (the buttons are called ex. NewCardButton2 pointing to team2)
-        var buttonID = event.target.id;
-        var ID = buttonID[buttonID.length -1];
-        console.log(this);
-        console.log(buttonID);
-        console.log(ID);
+        var getButton = event.target.id;
+        var getButtonId = getButton[getButton.length -1];
+       
+        teamArray.push({getButtonId, boxIterator});
 
         // creating a new card
         var newCard = document.createElement("div");
-        newCard.className = 'NewCard';
-        newCard.id = 'NewCard' + boxI;
-        boxI++;
+        newCard.className = "newCard";
+        newCard.id = "newCard" + getButtonId + teamArray[boxIterator].boxIterator;
 
         // name of the card
         var newCardName = document.createElement("p");
-
-        //NAME - Placeholder
-        newCardName.innerHTML = "New Card " + boxI;
-        newCardName.className = "NewCardName";
+        newCardName.innerHTML = "New Card " + getButtonId + teamArray[boxIterator].boxIterator;
+        newCardName.className = "newCardName";
         newCard.appendChild(newCardName);
-        document.getElementById("TeamBox"+ID).appendChild(newCard);
-        }
+
+        // new workorder
+        var newWork = document.createElement("button");
+        newWork.className = "newWork";
+        newWork.id = "newWorkButton" + getButtonId + teamArray[boxIterator].boxIterator;
+        newCard.appendChild(newWork);
+        document.getElementById("newTeam"+getButtonId).appendChild(newCard);
+        newWork.addEventListener("click",test);
+    
+        boxIterator++;
+        newCard.draggable = true;
+    }
+
+    function test(){
+        console.log(this);
+        var getCard = event.target.id;
+        var newWorkOrderName = prompt("workordername");
+        var newWorkOrder = document.createElement("div");
+        newWorkOrder.innerHTML = newWorkOrderName;
+        newWorkOrder.className = "newWorkOrder";
+        newWorkOrder.id = "newWorkOrder" + getCard[getCard.length -2] + getCard[getCard.length -1];
+        document.getElementById("newCard" + getCard[getCard.length -2] + getCard[getCard.length -1]).appendChild(newWorkOrder);
 
 
-
-    };
+    }
+};
