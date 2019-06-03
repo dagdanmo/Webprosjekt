fd0f8762aa7524277351ef557ffaf678e6176085
Mathias <Olberic@users.noreply.github.com>
2019-06-03 15:40:03 +0200

    Ver1 of infobutton

--- Hovedmappe/TablePageScript.js
+++ Hovedmappe/TablePageScript.js
@@ -243,4 +243,14 @@ window.onload = function(){
 
 
     }
+
+    var infoButton = document.getElementById("infoButtonImg");
+    var infoText = document.getElementById("hoverInfoText");
+
+    infoButton.addEventListener("mouseover", showInfoText);
+
+    function showInfoText(){
+        console.log("Hovering over infoButton");
+        infoText.innerText="Hehe";
+    }
 };
